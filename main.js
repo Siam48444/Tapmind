647e8edf1e303bb98b9585d8d14b1fbb2956e7e3
Siam <siam48444@gmail.com>
2024-06-22 17:04:58 +0600

    Main

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Lenis
 const lenis = new Lenis();
 
 lenis.on("scroll", ScrollTrigger.update);
@@ -7,3 +8,7 @@ gsap.ticker.add((time) => {
 });
 
 gsap.ticker.lagSmoothing(0);
+
+// Appear after load animations.
+gsap.from("nav", { opacity: 0, duration: 0.5, delay: 0.3, y: "-100%" });
+gsap.from(".appear_after_load", { opacity: 0, duration: 0.5, stagger: 0.05, delay: 0.3, y: 50 });
