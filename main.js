c36681fd26ac6acab324d0124e4f2d3c8542cfed
Siam <siam48444@gmail.com>
2024-06-23 16:37:23 +0600

    Main

--- main.js
+++ main.js
@@ -25,8 +25,8 @@ document.querySelectorAll(".primary_color_text").forEach((primary_text) => {
 // Navigation link color changing animations.
 const nav_link = document.querySelectorAll(".nav_link");
 
-const home = document.getElementById("home");
-const services_section = document.getElementById("services_section");
+const home = document.querySelector("#home");
+const services_section = document.querySelector("#services_section");
 
 if (services_section.getBoundingClientRect().top < 0) {
     nav_link.forEach((link) => link.classList.remove("nav_active"));
