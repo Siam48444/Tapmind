da6433949387d077cb5c1b3a8e03037ea5cfebc3
Siam <siam48444@gmail.com>
2024-06-23 11:04:04 +0600

    Main

--- main.js
+++ main.js
@@ -12,3 +12,8 @@ gsap.ticker.lagSmoothing(0);
 // Appear after load animations.
 gsap.from("nav", { duration: 0.5, delay: 0.3, y: "-100%", ease: "power1.out" });
 gsap.from(".appear_after_load", { opacity: 0, duration: 0.5, stagger: 0.1, delay: 0.3, y: 50, ease: "power1.out" });
+
+// Appear with scroll animations.
+document.querySelectorAll(".appear_with_scroll").forEach((appear) => {
+    gsap.from(appear, { opacity: 0, duration: 0.5, scale: 0.9, y: 50, scrollTrigger: { trigger: appear, start: "top 70%" } });
+});
