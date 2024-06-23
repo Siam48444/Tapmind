91ccba0a2b44f95df6a2ce389745b4e1b9ade746
Siam <siam48444@gmail.com>
2024-06-23 21:39:54 +0600

    Main

--- index.js
+++ index.js
@@ -8,5 +8,8 @@ for (let i = 0; i < about_btn.length; i++) {
     about_btn[i].addEventListener("click", () => {
         about_btn.forEach((btn) => btn.classList.remove("about_active"));
         about_btn[i].classList.add("about_active");
+
+        about_svg.forEach((svg) => svg.classList.remove("about_svg_active"));
+        about_svg[i].classList.add("about_svg_active");
     });
 }
