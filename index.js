f3d86820e534c0775ed7b51afb7ec1a190f3d07d
Siam <siam48444@gmail.com>
2024-06-23 21:27:03 +0600

    Main

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Nav link color changing.
+
+// About section interactivity.
+const about_btn = document.querySelectorAll(".about");
+const about_svg = document.querySelectorAll(".about_svg_div");
