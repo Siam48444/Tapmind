9c807e5aecd1b8030e77c60b8d427d71b7f33033
Siam <siam48444@gmail.com>
2024-06-23 21:35:19 +0600

    Main

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 // About section interactivity.
 const about_btn = document.querySelectorAll(".about");
 const about_svg = document.querySelectorAll(".about_svg_div");
+
+for (let i = 0; i < about_btn.length; i++) {
+    about_btn[i].addEventListener("click", () => {
+        about_btn.forEach((btn) => btn.classList.remove("about_active"));
+        about_btn[i].classList.add("about_active");
+    });
+}
