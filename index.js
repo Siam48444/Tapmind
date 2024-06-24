5810c05253dc7568d05a6b0455ad0c42c5a3550b
Siam <siam48444@gmail.com>
2024-06-24 13:11:18 +0600

    Main

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const about_btn = document.querySelectorAll(".about");
 const about_svg = document.querySelectorAll(".about_svg_div");
 
 for (let i = 0; i < about_btn.length; i++) {
+    about_svg[0].classList.add("about_svg_active");
+
     about_btn[i].addEventListener("click", () => {
         about_btn.forEach((btn) => btn.classList.remove("about_active"));
         about_btn[i].classList.add("about_active");
